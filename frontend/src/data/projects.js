1ce56daa04e228ccc2993fb68c1346b0942afb9d
getahun <getahunassefa277@gmail.com>
2026-06-25 21:04:00 +0300

    cv can be dowloaded directly

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -1,7 +1,7 @@
 import p1_img1 from "../assets/projects/project1/img1.png";
 import p1_img2 from "../assets/projects/project1/img2.png";
 import p1_img3 from "../assets/projects/project1/img3.png";
-import p1_video from "../assets/projects/project1/(ኢትዮጵያ) Ethiopan Ortodox Tewhido mezmur ሊቀ መዘምራን ቴዎድሮስ ዮሴፍ.mp4";
+// import p1_video from "../assets/projects/project1/(ኢትዮጵያ) Ethiopan Ortodox Tewhido mezmur ሊቀ መዘምራን ቴዎድሮስ ዮሴፍ.mp4";
 
 import p2_img1 from "../assets/projects/project2/img1.png";
 import p2_img2 from "../assets/projects/project2/img2.png";
@@ -15,7 +15,7 @@ export const projectsData = [
     description:
       "Designed and extended a campus network to a nearby building using VLANs, inter-VLAN routing, EtherChannel, and STP to improve reliability, segmentation, and performance.",
     highlights: ["VLAN Design", "Inter-VLAN Routing", "STP and EtherChannel"],
-    video: p1_video,
+    // video: p1_video,
     images: [p1_img1, p1_img2, p1_img3],
   },
   {
